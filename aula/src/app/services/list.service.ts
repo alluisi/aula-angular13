5ee8fbbef3bec6063e520ba47a34a493127dd4ee
alluisi <lecaluisi@hotmail.com>
2022-07-22 18:01:12 -0300

    aula 20/excluindo dados pelo service

--- aula/src/app/services/list.service.ts
+++ aula/src/app/services/list.service.ts
@@ -14,9 +14,8 @@ export class ListService {
 
   constructor(private http: HttpClient) { }
 
-  remove(animals: Animal[], animal: Animal) {
-    return animals.filter((a) => animal.name !== a.name);
-    // esse método foi utilizado aqui apenas para fins didáticos
+  remove(id: number) {
+    return this.http.delete<Animal>(`${this.apiUrl}/${id}`);
   }
 
   getAll(): Observable<Animal[]> {
